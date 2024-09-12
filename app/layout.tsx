bcec660883118555deaab8dba22dc2451eecce5a
Irefrans Cosme <72249653+irefranscosme@users.noreply.github.com>
2024-09-12 19:09:45 +0800

    added favicon and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,35 @@
-import { Container } from "@chakra-ui/react";
-import Header from "./sections/header";
-import { Providers } from "./providers";
+import { Container } from '@chakra-ui/react';
+import Header from './sections/header';
+import { Providers } from './providers';
+import Head from 'next/head';
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <Providers>
-          <Container maxW="150ch" paddingBlock="4" position="relative">
-            <Header />
-            {children}
-          </Container>
-        </Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <Head>
+                <link
+                    rel="icon"
+                    href="/DIGI-removebg-preview.ico"
+                    type="image/ico"
+                    sizes="any"
+                />
+            </Head>
+            <body>
+                <Providers>
+                    <Container
+                        maxW="150ch"
+                        paddingBlock="4"
+                        position="relative"
+                    >
+                        <Header />
+                        {children}
+                    </Container>
+                </Providers>
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,12 @@ import How from './sections/how';
 import Mission from './sections/mission';
 import Footer from './sections/footer';
 
+export const metadata = {
+    title: 'Digi',
+    description:
+        'Simplify your financial planning with DIGI where smart diversification meets effortless management.',
+};
+
 export default function Home() {
     return (
         <Flex flexDirection="column" gap="20">
