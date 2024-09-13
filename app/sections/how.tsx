b3b0e7e9481199f7f81121602ecc05cfaad9be8f
Irefrans Cosme <72249653+irefranscosme@users.noreply.github.com>
2024-09-13 18:36:52 +0800

    how it works layout

--- app/sections/how.tsx
+++ app/sections/how.tsx
@@ -1,11 +1,13 @@
 import {
-    Box,
+    Card,
+    CardBody,
     Flex,
+    Grid,
+    GridItem,
     Heading,
-    ListItem,
+    Image,
     Stack,
     Text,
-    UnorderedList,
 } from '@chakra-ui/react';
 import React from 'react';
 
@@ -16,70 +18,205 @@ const How = () => {
             position="relative"
             flexDirection="column"
             justifyContent={'center'}
+            gap="8"
         >
             <Flex justifyContent="center" alignItems="center">
-                <Box>
-                    <Heading textAlign="center">How it works?</Heading>
-                    <Text textAlign="center">
+                <Flex flexDirection="column">
+                    <Heading
+                        textAlign="center"
+                        lineHeight="1.272"
+                        letterSpacing="-0.02em"
+                        fontSize="1.618em"
+                        fontWeight={'bold'}
+                    >
+                        How it works?
+                    </Heading>
+                    <Text
+                        textAlign="center"
+                        flexBasis="50%"
+                        minWidth="28em"
+                        maxWidth="60em"
+                        lineHeight="1.618"
+                        letterSpacing="-0.011em"
+                        fontSize="1em"
+                    >
                         Input your salary, create and select your
                         diversification categories, then set goals and review
                         your progress regularly to stay on track toward
                         financial growth and stability.
                     </Text>
-                </Box>
+                </Flex>
             </Flex>
-            <Flex>
-                <Stack>
-                    <Text>Step 1</Text>
-                    <Text>Input your salary</Text>
-                    <UnorderedList>
-                        <ListItem>
-                            <Text>
-                                Enter your total salary to start the process of
-                                diversification.
-                            </Text>
-                        </ListItem>
-                    </UnorderedList>
-                </Stack>
-                <Stack>
-                    <Text>Step 2</Text>
-                    <Text>Choose Your Diversification Categories</Text>
-                    <UnorderedList>
-                        <ListItem>
-                            <Text>
-                                Create custom categories for your salary, such
-                                as investments, savings, or side income.
-                            </Text>
-                        </ListItem>
-                        <ListItem>
-                            <Text>
-                                Select the categories to allocate portions of
-                                your salary accordingly.
-                            </Text>
-                        </ListItem>
-                    </UnorderedList>
-                </Stack>
-                <Stack>
-                    <Text>Step 3</Text>
-                    <Text>Set Goals and Review</Text>
-                    <UnorderedList>
-                        <ListItem>
-                            <Text>
-                                Define specific goals for each category, such as
-                                target savings amounts or investment growth.
-                            </Text>
-                        </ListItem>
-                        <ListItem>
-                            <Text>
-                                Review your allocations regularly to ensure they
-                                align with your financial goals and adjust as
-                                needed based on performance and changes in your
-                                financial situation.
-                            </Text>
-                        </ListItem>
-                    </UnorderedList>
-                </Stack>
-            </Flex>
+            <Grid
+                gap="4"
+                gridTemplateColumns={['auto', 'auto', 'repeat(3, 1fr)']}
+            >
+                <GridItem>
+                    <Card height={'100%'}>
+                        <CardBody>
+                            <Flex
+                                gap="4"
+                                alignItems="center"
+                                justifyContent="space-between"
+                            >
+                                <Heading
+                                    lineHeight="1.272em"
+                                    letterSpacing="-0.014em"
+                                    fontSize="1.129em"
+                                    opacity={'0.8'}
+                                >
+                                    Step 1
+                                </Heading>
+                                <Image
+                                    src="/assets/icons/user-salary.svg"
+                                    alt="types-of-diversification.png"
+                                    width={25}
+                                    height={25}
+                                />
+                            </Flex>
+                            <Flex gap="4" flexDirection="column">
+                                <Heading
+                                    lineHeight="1.618em"
+                                    letterSpacing="-0.011em"
+                                    fontSize="1.272em"
+                                    fontWeight="bold"
+                                >
+                                    Input your salary
+                                </Heading>
+                                <Stack gap="2">
+                                    <Text
+                                        lineHeight="1.618em"
+                                        letterSpacing="-0.011em"
+                                        fontSize="1em"
+                                        mt="-0.2em"
+                                    >
+                                        Enter your total salary to start the
+                                        process of diversification.
+                                    </Text>
+                                </Stack>
+                            </Flex>
+                        </CardBody>
+                    </Card>
+                </GridItem>
+                <GridItem>
+                    <Card height={'100%'}>
+                        <CardBody>
+                            <Flex
+                                gap="4"
+                                alignItems="center"
+                                justifyContent="space-between"
+                            >
+                                <Heading
+                                    lineHeight="1.272em"
+                                    letterSpacing="-0.014em"
+                                    fontSize="1.129em"
+                                    opacity={'0.8'}
+                                >
+                                    Step 2
+                                </Heading>
+                                <Image
+                                    src="/assets/icons/choose.svg"
+                                    alt="types-of-diversification.png"
+                                    width={25}
+                                    height={25}
+                                />
+                            </Flex>
+                            <Flex gap="4" flexDirection="column">
+                                <Heading
+                                    lineHeight="1.618em"
+                                    letterSpacing="-0.011em"
+                                    fontSize="1.272em"
+                                    fontWeight="bold"
+                                >
+                                    Choose Your Diversification Categories
+                                </Heading>
+                                <Stack gap="2">
+                                    <Text
+                                        lineHeight="1.618"
+                                        letterSpacing="-0.011em"
+                                        fontSize="1em"
+                                        mt="-0.2em"
+                                    >
+                                        Create custom categories for your
+                                        salary, such as investments, savings, or
+                                        side income.
+                                    </Text>
+                                    <Text
+                                        lineHeight="1.618"
+                                        letterSpacing="-0.011em"
+                                        fontSize="1em"
+                                        mt="-0.2em"
+                                    >
+                                        Select the categories to allocate
+                                        portions of your salary accordingly.
+                                    </Text>
+                                </Stack>
+                            </Flex>
+                        </CardBody>
+                    </Card>
+                </GridItem>
+                <GridItem>
+                    <Card height={'100%'}>
+                        <CardBody>
+                            <Flex
+                                gap="4"
+                                alignItems="center"
+                                justifyContent="space-between"
+                            >
+                                <Heading
+                                    lineHeight="1.272em"
+                                    letterSpacing="-0.014em"
+                                    fontSize="1.129em"
+                                    opacity={'0.8'}
+                                >
+                                    Step 3
+                                </Heading>
+                                <Image
+                                    src="/assets/icons/thumbs-up-trust.svg"
+                                    alt="types-of-diversification.png"
+                                    width={25}
+                                    height={25}
+                                />
+                            </Flex>
+                            <Flex gap="4" flexDirection="column">
+                                <Heading
+                                    lineHeight="1.618em"
+                                    letterSpacing="-0.011em"
+                                    fontSize="1.272em"
+                                    fontWeight="bold"
+                                >
+                                    Set Goals and Review
+                                </Heading>
+
+                                <Stack gap="2">
+                                    <Text
+                                        lineHeight="1.618"
+                                        letterSpacing="-0.011em"
+                                        fontSize="1em"
+                                        mt="-0.2em"
+                                    >
+                                        Define specific goals for each category,
+                                        such as target savings amounts or
+                                        investment growth.
+                                    </Text>
+                                    <Text
+                                        lineHeight="1.618"
+                                        letterSpacing="-0.011em"
+                                        fontSize="1em"
+                                        mt="-0.2em"
+                                    >
+                                        Review your allocations regularly to
+                                        ensure they align with your financial
+                                        goals and adjust as needed based on
+                                        performance and changes in your
+                                        financial situation.
+                                    </Text>
+                                </Stack>
+                            </Flex>
+                        </CardBody>
+                    </Card>
+                </GridItem>
+            </Grid>
         </Flex>
     );
 };
