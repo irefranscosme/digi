e061b26aac22363359bf4777159d35eddb1da23e
Irefrans Cosme <72249653+irefranscosme@users.noreply.github.com>
2024-09-14 15:50:45 +0800

    active link revamp

--- app/components/active-link.tsx
+++ app/components/active-link.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import { Link as ChakraLink, LinkProps } from '@chakra-ui/react';
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
 
 interface NavLinkProps extends LinkProps {
     children?: string | React.ReactNode;
@@ -19,39 +17,27 @@ function NavLink({
     color = 'brand.primary',
     ...props
 }: NavLinkProps) {
-    const [isMounted, setIsMounted] = useState(false); // To track if we're on the client
     const pathname = usePathname();
     const isActive = pathname === to;
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-
-    if (!isMounted) {
-        return null;
-    }
-
     if (isActive) {
         return (
-            <Link href={to}>
-                <ChakraLink
-                    {...props}
-                    {...activeProps}
-                    _hover={{ color: 'selected' }}
-                    color={color}
-                >
-                    {children}
-                </ChakraLink>
-            </Link>
+            <ChakraLink
+                href={to}
+                {...props}
+                {...activeProps}
+                _hover={{ color: 'selected' }}
+                color={color}
+            >
+                {children}
+            </ChakraLink>
         );
     }
 
     return (
-        <Link href={to}>
-            <ChakraLink {...props} _hover={{ color: 'selected' }}>
-                {children}
-            </ChakraLink>
-        </Link>
+        <ChakraLink href={to} {...props} _hover={{ color: 'selected' }}>
+            {children}
+        </ChakraLink>
     );
 }
 
