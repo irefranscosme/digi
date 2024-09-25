9b29c7bc808325f3e0f139788622680892b535a1
Irefrans Cosme <72249653+irefranscosme@users.noreply.github.com>
2024-09-25 22:15:40 +0800

    fix users can access protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+import { PROTECTED_ROUTES } from '@/protected-routes';
 import { createClient } from '@/utils/supabase/server';
 import { NextResponse, type NextRequest } from 'next/server';
 
@@ -18,7 +19,7 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/dashboard', request.url));
     }
 
-    if (!user && request.nextUrl.pathname === '/dashboard') {
+    if (!user && PROTECTED_ROUTES.includes(request.nextUrl.pathname)) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
 
@@ -27,6 +28,8 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
+        '/dashboard',
+        '/income-streams',
         '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 };
